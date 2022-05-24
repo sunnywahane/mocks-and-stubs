bb491b59dfe1eb9f3c5d654b047d09f1c1d2d544
Sunny Wahane <sunnywahane@Apples-MacBook-Pro-6.local>
2022-05-24 17:57:51 +0530

    Added test to check the sendWelcomeEmail functionality

---
+++ account-details/test/UserServiceTest.kt
@@ -0,0 +1,18 @@
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import models.EmailBody
+import service.UserService
+
+class UserServiceTest: StringSpec() {
+
+    init {
+        "testing welcome email service" {
+            val dummyService = dummyEmailService()
+            val userService = UserService(null, dummyService)
+            userService.sendWelcomeEmail("test@gmail.com")
+            val expectedEmailBody = EmailBody("Welcome", "Welcome to the portal", "test@gmail.com")
+            val actualEmailBody = dummyService.getLastEmail()
+            actualEmailBody shouldBe expectedEmailBody
+        }
+    }
+}
